76ac02251f5177ed911e1347a43c8e29e98f4426
giuseppe romano <giuseppenadir.romano@gmail.com>
2022-02-02 16:13:33 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule
